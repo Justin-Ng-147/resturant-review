94839d3168a0365e456234423c8e74897e2dce84
Justin <justin.ng147@gmail.com>
2021-09-22 16:41:19 -0700

    created express app and added .env with mongodb info

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+// import resturants from ".api/resturants.route.js"
+import airbnb from "./api/airbnb.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/airbnb", airbnb)
+app.use("*", (req,res) => res.status(404).json({error: "not found"}))
+
+export default app
